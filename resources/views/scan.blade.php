575e87506d4b1806c3e775e72d70adbde778b93c
Daniela Villanueva Meza <A323213@alumnos.uaslp.mx>
2024-09-12 09:03:39 -0600

    prueba tailwind

---
+++ resources/views/scan.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  @vite('resources/css/app.css')
+</head>
+<body class="antialiased">
+<div class="bg-white min-h-screen flex ">
+  <div class="relative w-full max-w-lg">
+    <div class="absolute bottom-0 left-0 w-0 h-0 
+                border-l-[10rem] border-l-transparent
+                border-b-[25rem] border-b-purple-300 mix-blend-multiply opacity-70
+                border-r-[10rem] border-r-transparent"></div>
+    <div class="absolute bottom-0 left-80 w-0 h-0 
+                border-l-[10rem] border-l-transparent
+                border-b-[25rem] border-b-purple-300 mix-blend-multiply opacity-70
+                border-r-[10rem] border-r-transparent"></div>
+    <div class="absolute bottom-0 -right-0 w-0 h-0 
+                border-l-[10rem] border-l-transparent
+                border-b-[25rem] border-b-purple-300 mix-blend-multiply opacity-70
+                border-r-[10rem] border-r-transparent"></div>
+    
+    
+                <div class="m-8 relative space-y-4">
+      <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+        <div class="flex-1">
+          <div class="h-4 w-48 bg-gray-300 rounded"></div>
+        </div>
+        <div>
+          <div class="w-24 h-6 rounded-lg bg-purple-300"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+    return ({ opacityValue }) => {
+        if (opacityValue !== undefined) {
+            return `rgba(var(${variableName}), ${opacityValue})`;
+        }
+        return `rgb(var(${variableName}))`;
+    };
+}
+
 export default {
-    content: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
     theme: {
-        extend: {},
+        extend: {
+            textColor: {
+                skin: {
+                    base: withOpacity("--color-text-base"),
+                    muted: withOpacity("--color-text-muted"),
+                    inverted: withOpacity("--color-text-inverted"),
+                },
+            },
+            backgroundColor: {
+                skin: {
+                    fill: withOpacity("--color-fill"),
+                    "button-accent": withOpacity("--color-button-accent"),
+                    "button-accent-hover": withOpacity(
+                        "--color-button-accent-hover"
+                    ),
+                    "button-muted": withOpacity("--color-button-muted"),
+                },
+            },
+            gradientColorStops: {
+                skin: {
+                    hue: withOpacity("--color-fill"),
+                },
+            },
+        },
     },
     plugins: [],
 };
